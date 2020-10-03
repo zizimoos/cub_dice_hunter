2452e1cecf89146a341390e65d5a5d53dc1d7ade
zizimoos_iMac <zizimoos@gmail.com>
2020-10-03 15:27:37 +0900

    Updates

---
+++ precache-manifest.789bc7370ac5508310e2012a903bce0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afc2ff22547d732ebd6c27f9e82ce5",
+    "url": "/cub_dice_hunter/index.html"
+  },
+  {
+    "revision": "79192c0d6dbbd104af7d",
+    "url": "/cub_dice_hunter/static/js/2.ed6ef188.chunk.js"
+  },
+  {
+    "revision": "81284605075d720d4aa1f746bd73ef05",
+    "url": "/cub_dice_hunter/static/js/2.ed6ef188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4b8bace78df2502936",
+    "url": "/cub_dice_hunter/static/js/main.0a9e6dd2.chunk.js"
+  },
+  {
+    "revision": "72ccac35f78d1330e537",
+    "url": "/cub_dice_hunter/static/js/runtime-main.4e4fb53e.js"
+  }
+]);
