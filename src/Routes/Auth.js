7e689e99c21566bb20a87081460902aabd59fc6c
zizimoos_iMac <zizimoos@gmail.com>
2020-10-04 21:27:37 +0900

    history push

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -111,7 +111,7 @@ const Auth = ({ history }) => {
           console.log("이미 다른 곳에서 접속중입니다.");
           setError("이미 다른 곳에서 접속중입니다.");
           setLogin({ loggIn: false });
-          history.replace("/production");
+          history.push("/production");
         } else if (check.length === 0) {
           await authService.signInWithEmailAndPassword(email, password);
           setError("로그인 중입니다.");
