0d81dc475d7ac25d0db8ee72a7977cdbd5bffeb3
zizimoos_iMac <zizimoos@gmail.com>
2020-10-14 23:17:19 +0900

    ....

--- src/Routes/AuthAdmin.js
+++ src/Routes/AuthAdmin.js
@@ -98,21 +98,21 @@ const Auth = () => {
     console.log("autoGenID");
     try {
       if (true) {
-        for (let i = 0; i < 1000; i++) {
+        for (let i = 112; i < 300; i++) {
           if (i < 10) {
-            let aemail = "winner_000" + i + "@busta.com";
+            let aemail = "cheer_000" + i + "@busta.com";
             let apassword =
               "victory" + i * Math.floor(Math.random() * 1000 + 111);
             console.log("id :" + aemail, "password :" + apassword);
             await authService.createUserWithEmailAndPassword(aemail, apassword);
           } else if (i > 9 && i < 100) {
-            let aemail = "winner_00" + i + "@busta.com";
+            let aemail = "cheer_00" + i + "@busta.com";
             let apassword =
               "victory" + i * Math.floor(Math.random() * 1000 + 111);
             console.log("id :" + aemail, "password :" + apassword);
             await authService.createUserWithEmailAndPassword(aemail, apassword);
           } else if (i > 99 && i < 1000) {
-            let aemail = "winner_0" + i + "@busta.com";
+            let aemail = "cheer_0" + i + "@busta.com";
             let apassword =
               "victory" + i * Math.floor(Math.random() * 1000 + 111);
             console.log("id :" + aemail, "password :" + apassword);
